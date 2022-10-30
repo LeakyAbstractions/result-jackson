fc3f607e6c34efdf8a8dedfda6b261bf96aaf84a
Guillermo Calvo <guillermo@guillermo.dev>
2022-10-30 18:28:48 +0100

    Update documentation

--- lib-jackson/src/main/java/com/leakyabstractions/result/jackson/package-info.java
+++ lib-jackson/src/main/java/com/leakyabstractions/result/jackson/package-info.java
@@ -1,10 +1,8 @@
 /**
- * A Java library to handle success and failure without exceptions
- * <h2>Lazy Result Library for Java</h2>
+ * Jackson Datatype Module for Result
  * <p>
- * Lazy results can be manipulated just like any other result; they will try to defer the invocation of the given
- * supplier as long as possible. The purpose is to encapsulate an expensive operation that may be omitted if there's no
- * actual need to examine the result.
+ * This library provides a <a href="https://github.com/FasterXML/jackson">Jackson datatype module</a> for
+ * {@link com.leakyabstractions.result.Result} objects.
  *
  * @author Guillermo Calvo
  * @see com.leakyabstractions.result
